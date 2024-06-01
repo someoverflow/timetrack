40b20e0a42ce61a0ea356ec6e5e4301f71d7be44
someoverflow <someoverflow@someworkflow.de>
2024-06-01 21:28:53 +0200

    refactor(ui improvements): Refine font and toasts
    - Use JetBrains Mono
    - Move Toasts to top right

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,23 @@
 // UI
 import { Toaster } from "@/components/ui/sonner";
 import NextTopLoader from "nextjs-toploader";
+import { JetBrains_Mono, Ubuntu_Mono } from "next/font/google";
 
 // Provider
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { SessionProvider, ThemeProvider } from "@/lib/provider";
 
 import type { Metadata } from "next";
-
 export const metadata: Metadata = {
 	title: "Time Track",
 	description: "Track your Time",
 };
+
+const mono = JetBrains_Mono({
+	variable: "--mono-font",
+	subsets: ["latin-ext"],
+	display: "swap",
+});
 
 import "./globals.css";
 import "animate.css";
@@ -22,14 +28,14 @@ export default async function RootLayout({
 	children: React.ReactNode;
 }) {
 	return (
-		<html lang="en" suppressHydrationWarning>
+		<html lang="en" className={mono.variable} suppressHydrationWarning>
 			<body>
 				<NextTopLoader showSpinner={false} />
 				<ThemeProvider attribute="class" defaultTheme="system" enableSystem>
 					<SessionProvider>
 						<TooltipProvider delayDuration={100}>
 							{children}
-							<Toaster />
+							<Toaster position="top-right" />
 						</TooltipProvider>
 					</SessionProvider>
 				</ThemeProvider>
