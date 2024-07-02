9ba7b86b2d3fed01bc00c9c85e48e620fe2d8f8f
someoverflow <someoverflow@someworkflow.de>
2024-07-03 00:41:59 +0200

    feat(timer page): Use custom hook
    - Create a custom hook with all functions from timer section
    - Refactor timer section file

--- src/app/timer-section.tsx
+++ src/app/timer-section.tsx
@@ -1,19 +1,12 @@
 "use client";
 
-import { useCallback, useEffect, useReducer, useState } from "react";
+// Types
+import type { Time } from "@prisma/client";
 
+// UI
 import { Button } from "@/components/ui/button";
 import { Separator } from "@/components/ui/separator";
 import { Skeleton } from "@/components/ui/skeleton";
-import {
-	AlertDialog,
-	AlertDialogCancel,
-	AlertDialogContent,
-	AlertDialogDescription,
-	AlertDialogFooter,
-	AlertDialogHeader,
-	AlertDialogTitle,
-} from "@/components/ui/alert-dialog";
 import {
 	Tooltip,
 	TooltipContent,
@@ -21,198 +14,15 @@ import {
 } from "@/components/ui/tooltip";
 import { Card, CardContent, CardHeader } from "@/components/ui/card";
 
-import { toast } from "sonner";
+import { PlayCircle, RefreshCw, StopCircle, X } from "lucide-react";
 
-import { PlayCircle, RefreshCcw, RefreshCw, StopCircle, X } from "lucide-react";
-import { getTimePassed } from "@/lib/utils";
-import Link from "next/link";
+// Utils
 import { useTranslations } from "next-intl";
 
-interface timerSectionState {
-	firstRun: boolean;
-	loaded: boolean;
-	running: boolean;
-	changeModal: boolean;
-	changeTimer: string | undefined;
-	stop: boolean;
-	delay: number;
-}
+import useLiveTimer from "@/lib/hooks/useLiveTimer";
 
 export default function TimerSection() {
-	const t = useTranslations("Timer");
-
-	const [data, updateData] = useReducer(
-		(prev: timerSectionState, next: Partial<timerSectionState>) => ({
-			...prev,
-			...next,
-		}),
-		{
-			firstRun: false,
-			loaded: false,
-			running: false,
-			changeModal: false,
-			changeTimer: undefined,
-			stop: false,
-			delay: 60,
-		},
-	);
-
-	const [currentTimer, setCurrentTimer] = useState<Timer>();
-	const [fetchedTimer, setFetchedTimer] = useState<Timer>();
-
-	const count = useCallback(() => {
-		if (!data.running) return;
-		if (!currentTimer && !fetchedTimer) return;
-
-		const result = JSON.parse(
-			JSON.stringify(currentTimer ? currentTimer : fetchedTimer),
-		);
-
-		const startDate = new Date(
-			Date.parse(fetchedTimer ? fetchedTimer.start : result.start),
-		);
-		startDate.setMilliseconds(0);
-		const currentDate = new Date();
-		currentDate.setMilliseconds(0);
-
-		const timePassed = getTimePassed(startDate, currentDate);
-
-		result.state = "stop";
-
-		result.start = startDate.toLocaleTimeString();
-		result.end = currentDate.toLocaleTimeString();
-
-		result.time = timePassed;
-
-		setCurrentTimer(result);
-	}, [currentTimer, fetchedTimer, data.running]);
-
-	const fetchCurrentTimer = useCallback(() => {
-		fetch("/api/times")
-			.then((result) => result.json())
-			.then((result) => {
-				if (result.success === false) {
-					updateData({
-						stop: true,
-					});
-					toast.error(`An error occurred while updating (${result.type})`, {
-						description: "Reloading the page could solve the problem",
-					});
-					return;
-				}
-
-				if (!data.firstRun) {
-					updateData({
-						firstRun: true,
-						loaded: true,
-					});
-				}
-
-				if (result.result?.end === null) {
-					const timer: Timer = result.result;
-					setFetchedTimer(timer);
-
-					updateData({
-						running: true,
-					});
-				} else {
-					updateData({
-						running: false,
-					});
-
-					setFetchedTimer(undefined);
-					setCurrentTimer(undefined);
-				}
-				count();
-			})
-			.catch((e) => {
-				updateData({
-					stop: true,
-				});
-				toast.error("An error occurred while updating", {
-					description: "Reloading the page could solve the problem",
-				});
-				console.error(e);
-			});
-	}, [data.firstRun, count]);
-
-	function toggleTimer(start: boolean) {
-		updateData({
-			loaded: false,
-		});
-
-		const data: Partial<Timer> = {
-			id: "<0>",
-			start: new Date(new Date().setSeconds(0)).toISOString(),
-			startType: "Website",
-		};
-
-		if (start) {
-			setFetchedTimer(data as Timer);
-			setCurrentTimer(undefined);
-
-			count();
-
-			updateData({
-				running: true,
-			});
-		} else {
-			updateData({
-				changeModal: true,
-				changeTimer: currentTimer?.id,
-				running: false,
-			});
-
-			setFetchedTimer(undefined);
-			setCurrentTimer(undefined);
-		}
-
-		fetch(`/api/times/toggle?type=Website&fixTime=${data.start}`, {
-			method: "PUT",
-		})
-			.then((result) => result.json())
-			.then(() => {
-				updateData({
-					loaded: true,
-				});
-				if (start) fetchCurrentTimer();
-			})
-			.catch((e) => {
-				updateData({
-					stop: true,
-				});
-				toast.warning(
-					`An error occurred while ${start ? "starting" : "stopping"}`,
-					{
-						description: "Reloading the page could solve the problem",
-					},
-				);
-				console.error(e);
-			});
-	}
-
-	// First Effect
-	// biome-ignore lint/correctness/useExhaustiveDependencies: Only run once
-	useEffect(() => {
-		fetchCurrentTimer();
-	}, []);
-
-	// Timer Effect
-	useEffect(() => {
-		const intervalId = setInterval(() => {
-			if (!data.stop) {
-				updateData({ delay: data.delay + 1 });
-				if (data.delay > 60) {
-					data.delay = 0;
-					updateData({ delay: 0 });
-					fetchCurrentTimer();
-					console.log("fetch", "new data");
-				}
-				if (data.running) count();
-			}
-		}, 250);
-		return () => clearInterval(intervalId);
-	}, [data, data.stop, data.running, data.delay, count, fetchCurrentTimer]);
+	const { timer, state, toggle } = useLiveTimer();
 
 	return (
 		<>
@@ -221,50 +31,23 @@ export default function TimerSection() {
 					<CardHeader>
 						<div className="w-full flex justify-center items-center flex-row gap-2">
 							<ToggleSection
-								loaded={data.loaded}
-								running={data.running}
-								startType={`${currentTimer?.startType}`}
-								toggleTimer={toggleTimer}
+								loaded={!state.loading}
+								running={state.running}
+								startType={`${timer?.startType}`}
+								toggleTimer={toggle}
 							/>
 						</div>
 					</CardHeader>
 					<CardContent>
 						<div className="w-full h-full flex flex-col items-center gap-6 pb-2">
 							<h1 className="text-5xl font-bold font-mono select-none animate__animated animate__fadeIn">
-								{data.running && currentTimer?.time
-									? currentTimer?.time
-									: "00:00:00"}
+								{state.running && timer?.time ? timer?.time : "00:00:00"}
 							</h1>
-							<CurrentTimerTime
-								running={data.running}
-								currentTimer={currentTimer}
-							/>
+							<TimeSection running={state.running} timer={timer} />
 						</div>
 					</CardContent>
 				</Card>
 			</div>
-
-			<AlertDialog
-				open={data.changeModal}
-				onOpenChange={(e) => updateData({ changeModal: e })}
-			>
-				<AlertDialogContent className="max-w-sm w-[95vw] rounded-sm">
-					<AlertDialogHeader>
-						<AlertDialogTitle>{t("Dialogs.Stopped.title")}</AlertDialogTitle>
-						<AlertDialogDescription>
-							{t("Dialogs.Stopped.description")}
-						</AlertDialogDescription>
-					</AlertDialogHeader>
-					<AlertDialogFooter>
-						<AlertDialogCancel>{t("Dialogs.Stopped.cancel")}</AlertDialogCancel>
-						<Button asChild>
-							<Link href={`/history?edit=${data.changeTimer}`} prefetch={false}>
-								{t("Dialogs.Stopped.edit")}
-							</Link>
-						</Button>
-					</AlertDialogFooter>
-				</AlertDialogContent>
-			</AlertDialog>
 		</>
 	);
 }
@@ -334,22 +117,22 @@ function ToggleSection({
 	);
 }
 
-function CurrentTimerTime({
+function TimeSection({
 	running,
-	currentTimer,
+	timer,
 }: {
 	running: boolean;
-	currentTimer: Timer | undefined;
+	timer: Time | undefined;
 }) {
-	if (running && currentTimer) {
+	if (running && timer) {
 		return (
 			<div className="flex w-full justify-center items-center gap-4">
 				<p className="text-muted-foreground text-center tabular-nums h-6 w-1/4 rounded-md animate__animated animate__fadeIn">
-					{`${currentTimer.start}`}
+					{`${timer.start.toLocaleTimeString()}`}
 				</p>
 				<Separator orientation="horizontal" className="w-5" />
 				<p className="text-muted-foreground text-center tabular-nums h-6 w-1/4 rounded-md animate__animated animate__fadeIn select-none">
-					{`${currentTimer.end}`}
+					{`${timer.end?.toLocaleTimeString()}`}
 				</p>
 			</div>
 		);
---
+++ src/lib/hooks/useLiveTimer.ts
@@ -0,0 +1,183 @@
+import type { Time } from "@prisma/client";
+import {
+	useCallback,
+	useDebugValue,
+	useEffect,
+	useReducer,
+	useState,
+} from "react";
+import { toast } from "sonner";
+import { getTimePassed } from "../utils";
+
+type stateReducerType = { error: boolean; loading: boolean; running: boolean };
+const stateReducer = (
+	state: stateReducerType,
+	action:
+		| { type: "error"; toast?: string | undefined }
+		| { type: "loading" | "running"; value: boolean },
+): stateReducerType => {
+	switch (action.type) {
+		case "error":
+			toast.error(action.toast ?? "An error occurred while updating", {
+				description: "Reloading the page could solve the problem",
+			});
+			return {
+				...state,
+				error: true,
+			};
+		case "loading":
+			return {
+				...state,
+				loading: action.value,
+			};
+		case "running":
+			return {
+				...state,
+				running: action.value,
+			};
+		default:
+			return {
+				...state,
+			};
+	}
+};
+
+const generateTimer = (): Time => {
+	return {
+		id: "string",
+		userId: null,
+
+		start: new Date(),
+		startType: "Website",
+
+		end: null,
+		endType: null,
+
+		time: null,
+		notes: null,
+		traveledDistance: null,
+		materials: "[]",
+		projectName: null,
+	};
+};
+
+export default function useLiveTimer() {
+	const [timer, setTimer] = useState<Time | undefined>(undefined);
+	const [state, dispatch] = useReducer(stateReducer, {
+		error: false,
+		loading: true,
+
+		running: false,
+	});
+
+	const fetchLatest = useCallback(async () => {
+		const fetchResult: APIResult | undefined = await fetch("/api/times")
+			.then((result) => result.json())
+			.catch((e) => {
+				console.error(e);
+				return undefined;
+			});
+		if (!fetchResult || fetchResult.type !== "ok") {
+			dispatch({ type: "error" });
+			return;
+		}
+
+		const result = fetchResult.result;
+
+		if (result) {
+			const start = new Date(result.start);
+			const end = result.end ? new Date(result.end) : new Date();
+
+			setTimer({
+				...result,
+				start: start,
+				end: end,
+				time: getTimePassed(start, end),
+			});
+		} else setTimer(undefined);
+
+		dispatch({ type: "running", value: result && !result.end });
+		dispatch({ type: "loading", value: false });
+	}, []);
+
+	const calculate = useCallback(() => {
+		if (!timer || !state.running || state.error) return;
+
+		const startDate = timer.start;
+		startDate.setMilliseconds(0);
+
+		const currentDate = new Date();
+		currentDate.setMilliseconds(0);
+
+		const timePassed = getTimePassed(startDate, currentDate);
+
+		setTimer({
+			...timer,
+			start: startDate,
+			end: currentDate,
+			time: timePassed,
+		});
+	}, [timer, state]);
+
+	const toggle = useCallback(
+		async (start: boolean) => {
+			dispatch({ type: "loading", value: true });
+
+			const tempTimer = generateTimer();
+			setTimer(start ? tempTimer : undefined);
+			dispatch({ type: "running", value: start });
+
+			const apiResult: APIResult | undefined = await fetch(
+				`/api/times/toggle?type=Website&fixTime=${tempTimer.start.toISOString()}`,
+				{
+					method: "PUT",
+				},
+			)
+				.then((result) => result.json())
+				.catch((e) => {
+					console.error(e);
+					return undefined;
+				});
+			if (!apiResult) {
+				dispatch({
+					type: "error",
+					toast: `An error occurred while ${start ? "starting" : "stopping"}`,
+				});
+				return;
+			}
+
+			dispatch({ type: "loading", value: false });
+			fetchLatest();
+		},
+		[fetchLatest],
+	);
+
+	// biome-ignore lint/correctness/useExhaustiveDependencies: Only run once
+	useEffect(() => {
+		fetchLatest();
+	}, []);
+
+	// Calculation Interval
+	useEffect(() => {
+		const intervalId = setInterval(() => {
+			if (!state.error && state.running) calculate();
+		}, 250);
+		return () => clearInterval(intervalId);
+	}, [state.error, state.running, calculate]);
+
+	// Fetch Interval
+	useEffect(() => {
+		const intervalId = setInterval(() => {
+			if (!state.error) {
+				fetchLatest();
+				console.log("fetch", "new data");
+			}
+		}, 30 * 1000);
+		return () => clearInterval(intervalId);
+	}, [state.error, fetchLatest]);
+
+	useDebugValue(timer);
+	useDebugValue(state);
+
+	return { timer, state, toggle };
+}
