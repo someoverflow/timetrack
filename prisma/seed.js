c4d54e03acfc0eab2d8ca89706ceebd289b95941
someoverflow <someoverflow@someworkflow.de>
2024-09-09 13:36:25 +0200

    fix(docker): db schema/seed

--- prisma/seed.js
+++ prisma/seed.js
@@ -4,32 +4,35 @@ const { hash } = require("bcryptjs");
 const prisma = new PrismaClient();
 
 async function main() {
-  const admin = await prisma.user.upsert({
-    where: { username: "admin" },
-    update: {},
-    create: {
-      username: "admin",
-      name: "Admin Account",
-      email: "admin@example.com",
-      password: await hash("changeme", 12),
-      role: "ADMIN",
-    },
-    select: {
-      id: true,
-      username: true,
-      name: true,
-      createdAt: true,
-      updatedAt: true,
-    },
-  });
-  console.log({ admin });
+	const password = await hash("changeme", 12);
+
+	const result = await prisma.user.upsert({
+		where: { username: "admin" },
+		update: {},
+		create: {
+			username: "admin",
+			name: "Admin Account",
+			email: "admin@example.com",
+			password: password,
+			role: "ADMIN",
+		},
+		select: {
+			id: true,
+			username: true,
+			name: true,
+			createdAt: true,
+			updatedAt: true,
+		},
+	});
+
+	console.log({ result });
 }
 main()
-  .then(async () => {
-    await prisma.$disconnect();
-  })
-  .catch(async (e) => {
-    console.error(e);
-    await prisma.$disconnect();
-    process.exit(1);
-  });
+	.then(async () => {
+		await prisma.$disconnect();
+	})
+	.catch(async (e) => {
+		console.error(e);
+		await prisma.$disconnect();
+		process.exit(1);
+	});
